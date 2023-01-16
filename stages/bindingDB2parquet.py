e65329e83dfd3c6957c15e4e21235a32d383be8a
ABorrel <a.borrel@gmail.com>
2023-01-16 21:31:09 +0000

    Fix error read tsv in python

--- stages/bindingDB2parquet.py
+++ stages/bindingDB2parquet.py
@@ -9,7 +9,7 @@ OutDirName = sys.argv[2]
 #read and convert in chunck
 i_name_parquet = 1
 chunksize = 10 ** 6
-with pd.read_fwf(InFileName, chunksize=chunksize,  sep='\t') as reader:
+with pd.read_csv(InFileName, chunksize=chunksize, sep = "\t", low_memory=False, error_bad_lines=False) as reader:
     for chunk in reader:
         chunk.to_parquet("%s/bindingDB%s.parquet"%(OutDirName, i_name_parquet))
         i_name_parquet = i_name_parquet + 1
